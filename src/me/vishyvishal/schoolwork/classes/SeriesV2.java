0f92c41579fbd830e2ecf06e5ad6b25cc42dd621
Vishal Pj <pjvishal200@gmail.com>
2021-11-30 10:27:41 +0530

    SeriesV2 addition

---
+++ src/me/vishyvishal/schoolwork/classes/SeriesV2.java
@@ -0,0 +1,23 @@
+package me.vishyvishal.schoolwork.classes;
+
+import java.util.Scanner;
+
+public class SeriesV2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i, a;
+        double s = 0;
+        System.out.println("Enter the value of a");
+        a = in.nextInt();
+        for (i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                s = s + Math.pow(a, i);
+
+            } else {
+                s = s - Math.pow(a, i);
+            }
+            System.out.println("The sum of series =" + s);
+        }
+    }
+}
+
